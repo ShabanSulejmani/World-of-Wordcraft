50963a595d7d44c774e8b3caae041f8c10f44a6b
Hultberg80 <hultberg80@gmail.com>
2025-01-09 11:48:10 +0100

    fetch_and_give_letters random word

--- Actions.cs
+++ Actions.cs
@@ -13,5 +13,18 @@ public class Actions
         db = database.Connection();
 
     }
-    
+
+    public async Task<Word> fetchAndGiveLetters()
+    {
+        await using var cmd = db.CreateCommand("Select ord from svenska_ord order by random() limit 1");
+        await using var reader = await cmd.ExecuteReaderAsync();
+        {
+            if (await reader.ReadAsync())
+            {
+                return new Word(reader.GetString(0), reader.GetString(1));
+            }
+        }
+        throw new InvalidOperationException("Inga ord hittades i databasen."); // Felhantering
+
+    }
 }
